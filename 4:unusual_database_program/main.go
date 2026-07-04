f99147dacf32f52757856d663e81c0b521b1d261
John <128350391+alpaOo@users.noreply.github.com>
2026-07-04 16:46:40 +0800

    add final solution iteration
    When processing UDP connections, do them sequentially; process packets in order of arrival. Do not spawn separate goroutines. This is different from TCP

--- 4:unusual_database_program/main.go
+++ 4:unusual_database_program/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"bytes"
 	"context"
-	"log"
 	"log/slog"
 	"net"
+	"net/netip"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 )
 
@@ -15,6 +16,13 @@ const ADDR = ":8080"
 
 type database struct {
 	table map[string]string
+	mu    sync.Mutex
+}
+
+func newDB() *database {
+	return &database{
+		table: map[string]string{},
+	}
 }
 
 // retrieveReq returns the concatenated key-value pair and a boolean that indicates if the
@@ -26,6 +34,9 @@ func (db *database) retrieveReq(key string) string {
 		kv := key + "=" + "Ken's key-value store V1.0"
 		return kv
 	}
+
+	db.mu.Lock()
+	defer db.mu.Unlock()
 
 	val, ok := db.table[key]
 	if !ok {
@@ -43,16 +54,10 @@ func (db *database) insertReq(key, val string) {
 		return
 	}
 
-	_, ok := db.table[key]
-	if !ok {
-		// Init new kv pair
-		db.table[key] = string(val)
-		return
-	}
+	db.mu.Lock()
+	defer db.mu.Unlock()
 
-	log.Println(db.table)
-
-	// Update to new value
+	// Update to new value or create one if key isn't found.
 	db.table[key] = string(val)
 }
 
@@ -61,6 +66,13 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	}))
+
+	// Init in-memory db.
+	db := newDB()
+
+	// Cleanup guys.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
 	// Init UDP listener.
 	udpAddr, err := net.ResolveUDPAddr("udp", ADDR)
@@ -74,52 +86,46 @@ func main() {
 		logger.Error("listen UDP error", "err", err)
 		return
 	}
-
-	// Cleanup guys.
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
+	slog.Info("Starting UDP server...", "addr", udpAddr.String())
 
 	go func() {
 		<-ctx.Done()
 		udpConn.Close()
+		logger.Info("Shutting down server...")
 	}()
 
-	slog.Info("Starting UDP server...", "addr", udpAddr.String())
-
-	// Init in-memory db.
-	db := &database{
-		table: map[string]string{},
-	}
-
-	// Initialize a buffer with a size of 990 Bytes. As per spec, requests and responses
-	// must be shorter than 1000 Bytes.
-
-	// Try 1 KB first.
-	buf := make([]byte, 1000)
-
 	for {
+		// Initialize a buffer with a size of 990 Bytes. As per spec, requests and responses
+		// must be shorter than 1000 Bytes.
+		//
+		// Try 1 KB first.
+		buf := make([]byte, 1000)
+
 		n, addrPort, err := udpConn.ReadFromUDPAddrPort(buf)
 		if err != nil {
 			logger.Error("read from UDP", "err", err)
 			return
 		}
 
-		request := bytes.TrimSpace(buf[:n])
+		request := buf[:n]
 		logger.Info("message received", "from", addrPort.String(), slog.String("msg", string(request)))
 
-		pos := bytes.Index(request, []byte("="))
-		log.Println(pos)
-		if pos == -1 {
-			// pos is -1 when "=" is not found. If not found, it's a "retrieve" request.
-			kv := db.retrieveReq(string(request))
-			_, err := udpConn.WriteToUDPAddrPort([]byte(kv+"\n"), addrPort)
-			if err != nil {
-				logger.Error("write to UDP after retrieve()", "err", err)
-				continue
-			}
-		} else {
-			// else, an "insert" request.
-			db.insertReq(string(request[:pos]), string(request[pos+1:]))
+		worker(udpConn, addrPort, request, db, logger)
+	}
+}
+
+func worker(udpConn *net.UDPConn, addrPort netip.AddrPort, request []byte, db *database, logger *slog.Logger) {
+	before, after, found := bytes.Cut(request, []byte("="))
+	if !found {
+		// pos is -1 when "=" is not found. If not found, it's a "retrieve" request.
+		kv := db.retrieveReq(string(request))
+		_, err := udpConn.WriteToUDPAddrPort([]byte(kv), addrPort)
+		if err != nil {
+			logger.Error("write to UDP after retrieve()", "err", err)
+			return
 		}
+	} else {
+		// else, an "insert" request.
+		db.insertReq(string(before), string(after))
 	}
 }
